a364902535892db9df97e75387ed26ada8859185
Leo Chaves <leingas13@gmail.com>
2024-07-11 03:24:03 +0000

    first commit

---
+++ domain.js
@@ -0,0 +1,13 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+pronoun.forEach(articulo => {
+    
+    adjetivo.forEach(adj => {
+        sujeto.forEach(suj => {
+            console.log(articulo + adj) + ".com";
+        });
+    });
+
+});
